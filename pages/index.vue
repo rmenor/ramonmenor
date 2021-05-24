0627a0b9fd6ab16402390b9cf867b370af29c3fd
Ramón Menor <ramonmenor@gmail.com>
2021-05-25 00:42:59 +0200

    Actualización

--- pages/index.vue
+++ pages/index.vue
@@ -3,32 +3,31 @@
     <div>
       <Logo />
       <h1 class="title">
-        MiWeb
+        Ramón Menor
       </h1>
-      <div class="links">
+      <h3 class="subtitle">Desarrollo web con Nuxt.</h3>
+      <p>
+        Esta página esta desarrollada con Nuxtjs, un framework basado en vue.js
+        para realizar páginas de tipo SPA (Static Page Aplication) o SSR (Server
+        Side Rendering)
+      </p>
+      <br>
+      <p>
         <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
+          href="https://nuxtjs.org"
           target="_blank"
           rel="noopener noreferrer"
           class="button--grey"
         >
-          GitHub
+          Visita Nuxt.js
         </a>
-      </div>
+      </p>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {};
 </script>
 
 <style>
@@ -42,16 +41,8 @@ export default {}
 }
 
 .title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
